4661943334c88c6f8d320748d6af257cbef19a6b
Soniya07Singh <117883269+Soniya07Singh@users.noreply.github.com>
2025-04-20 06:33:39 +0530

    Delete Task1.3.py

--- Task1.3.py
+++
@@ -1,9 +0,0 @@
-# CALCULATING SIMPLE INTEREST
-p = 10000     
-r = 5              
-t = 3             
-si = (p*r*t)/100
-print("Simple Interest for 3 years is:", si)
-
-#-----------------------output-----------------------
-#Simple Interest for 3 years is: 1500.0
